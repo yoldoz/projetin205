ba82d8b45700d762ae18568df2019645c5c5ee69
yoldoz <78078014+yoldoz@users.noreply.github.com>
2021-02-28 21:32:15 +0100

    Exercice 8

---
+++ AIPlayer.java
@@ -0,0 +1,25 @@
+import java.io.Serializable;
+import java.util.List;
+
+public class AIPlayer extends Player {
+    /* **
+     * Attribut
+     */
+    private BattleShipsAI ai;
+
+    /* **
+     * Constructeur
+     */
+    public AIPlayer(Board ownBoard, Board opponentBoard, List<AbstractShip> ships) {
+        super(ownBoard, opponentBoard, ships);
+        ai = new BattleShipsAI(ownBoard, opponentBoard);
+    }
+
+    // TODO AIPlayer must not inherit "keyboard behavior" from player. Call ai instead.
+    public void putShips() {
+        ai.putShips(this.ships);
+    }
+    public Hit sendHit(int[] coord) {
+        return ai.sendHit(coord);
+    }
+}
